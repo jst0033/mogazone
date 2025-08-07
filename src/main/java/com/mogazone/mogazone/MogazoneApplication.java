583f4d7802a553b53355c3cb594e096ebae0f9ea
awasoft <jst000520@awasoft.co.kr>
2025-08-07 10:03:02 +0900

    chore: 프로젝트 초기 설정 완료

---
+++ src/main/java/com/mogazone/mogazone/MogazoneApplication.java
@@ -0,0 +1,13 @@
+package com.mogazone.mogazone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MogazoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MogazoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mogazone/mogazone/MogazoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mogazone.mogazone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MogazoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
